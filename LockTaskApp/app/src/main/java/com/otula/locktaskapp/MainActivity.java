5345e4047b102af1f5cd2e884b10d626ea02843d
diz <33259622+otula@users.noreply.github.com>
2022-03-07 20:56:15 +0200

    Android 11 updates -r9702.

--- LockTaskApp/app/src/main/java/com/otula/locktaskapp/MainActivity.java
+++ LockTaskApp/app/src/main/java/com/otula/locktaskapp/MainActivity.java
@@ -23,12 +23,17 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.media.AudioManager;
 import android.net.http.SslError;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.UserManager;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.view.WindowManager;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
@@ -58,14 +63,8 @@ import java.util.Collections;
  *  The admin can be removed from adb shell by executing the command dpm remove-active-admin com.otula.locktaskapp/.DeviceAdmin
  *  After this you should be able to uninstall the application normally or by using adb shell command (adb uninstall com.otula.locktaskapp or pm uninstall --user 0 com.otula.locktaskapp)
  */
-public class MainActivity extends AppCompatActivity implements View.OnSystemUiVisibilityChangeListener {
+public class MainActivity extends AppCompatActivity {
 	private static final String TAG = MainActivity.class.toString();
-	private static final int FLAGS = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-			| View.SYSTEM_UI_FLAG_FULLSCREEN
-			| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 	private static final int NIGHTLY_RESET_HOUR = 4;
 	private static final int NIGHTLY_RESET_MINUTES = 15;
 	private static final String SESSIONSTORAGE_USERNAME = "username"; // session storage key name
@@ -76,8 +75,8 @@ public class MainActivity extends AppCompatActivity implements View.OnSystemUiVi
 	private static final String URL = "https://example.org/index.html";
 	private DevicePolicyManager _dpm = null;
 	private WebView _webView = null;
-	private Handler _timerHandler = new Handler();
-	private Runnable _nightlyResetRunnable = new Runnable() {
+	private Handler _timerHandler = null;
+	private final Runnable _nightlyResetRunnable = new Runnable() {
 		@Override
 		public void run() {
 			loadStartPage();
@@ -89,59 +88,79 @@ public class MainActivity extends AppCompatActivity implements View.OnSystemUiVi
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		getWindow().getDecorView().setSystemUiVisibility(FLAGS);
+		_timerHandler = new Handler(Looper.getMainLooper());
 
 		DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
 		ComponentName mDPM = new ComponentName(this, DeviceAdmin.class);
 
-		if (dpm.isDeviceOwnerApp(this.getPackageName())) {
-			// get this app package name
-			String[] packages = {this.getPackageName()};
+		if (dpm.isDeviceOwnerApp(this.getPackageName())) { // check that the admin is set
+			String[] packages = {this.getPackageName()}; // get this app package name
 			// mDPM is the admin package, and allow the specified packages to lock task
 			dpm.setLockTaskPackages(mDPM, packages);
 			dpm.setLockTaskFeatures(mDPM, DevicePolicyManager.LOCK_TASK_FEATURE_NONE);
 			dpm.addUserRestriction(mDPM, UserManager.DISALLOW_CREATE_WINDOWS); // this will stop toasts!
-			startLockTask();
+			startLockTask();  // start lock task
 		} else {
 			Log.e(TAG, "Application is not device owner!");
 		}
 
+		getSupportActionBar().hide(); // hide action bar
+
 		setContentView(R.layout.activity_main);
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-		getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(this);
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) { // on Android 11+
+			getWindow().setDecorFitsSystemWindows(false);
+			WindowInsetsController controller = getWindow().getInsetsController();
+			if(controller != null) {
+				controller.hide(WindowInsets.Type.systemBars());
+				controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+			}
+		} else { // Android <= 10
+			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_FULLSCREEN
+					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+			getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() { // track visibility changes to hide system bars when they are opened
+				@Override
+				public void onSystemUiVisibilityChange(int visibility) { // hide navigation bar if it is opened
+					if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+						getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+								| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+								| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+								| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+								| View.SYSTEM_UI_FLAG_FULLSCREEN
+								| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY); // close any status & notification bars if they become visible
+					}
+				}
+			});
+		}
+
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // we can also set screen on manually vs. AndroidManifest.xml
 
 		muteVolume();
 
+		initializeWebView();
+	}
+
+	/**
+	 *
+	 */
+	private void initializeWebView(){
 		_webView = findViewById(R.id.webview);
 		WebSettings webSettings = _webView.getSettings();
 		webSettings.setJavaScriptEnabled(true);
 		webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
 		webSettings.setDomStorageEnabled(true);
 		_webView.setWebChromeClient(new WebChromeClient());
-		_webView.setWebViewClient(new WebClient(){
-			@Override
-			public void onPageFinished(WebView view, String url) {
-				Log.d(TAG, "Page finished.");
-				view.invalidate();
-				_webView.refreshDrawableState();
-				super.onPageFinished(view, url);
-			}
+		_webView.setWebViewClient(new WebClient());
+	}
 
-			@Override
-			public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-				//super.onReceivedSslError(view, handler, error);
-				handler.proceed(); // skip SSL errors
-			}
-
-			@Override
-			public void onPageStarted(WebView view, String url, Bitmap favicon) {
-				Log.d(TAG, "Injecting username "+USERNAME+" and password "+PASSWORD_HIDDEN);
-				/* inject data to HTML5 SessionStorage, in this, username and password, we could also run other arbitrary JavaScript code after the page has finished loading if needed */
-				view.evaluateJavascript("sessionStorage.setItem(\'"+SESSIONSTORAGE_USERNAME+"\', \'"+USERNAME+"\')", null); // set credentials to override login page
-				view.evaluateJavascript("sessionStorage.setItem(\'"+SESSIONSTORAGE_PASSWORD+"\', \'"+PASSWORD+"\')", null); // set credentials to override login page
-				super.onPageStarted(view, url, favicon);
-			}
-		});
+	@Override
+	public void onBackPressed() {
+		// _webView.goBack(); // go back in webview
 	}
 
 	/**
@@ -202,15 +221,8 @@ public class MainActivity extends AppCompatActivity implements View.OnSystemUiVi
 		am.adjustStreamVolume(AudioManager.STREAM_RING, AudioManager.ADJUST_MUTE, 0);
 	}
 
-	@Override
-	public void onSystemUiVisibilityChange(int visibility) {
-		if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
-			getWindow().getDecorView().setSystemUiVisibility(FLAGS); // close any status & notification bars if they become visible
-		}
-	}
-
 	/**
-	 *
+	 * Simple implementation of WebViewClient
 	 */
 	private class WebClient extends WebViewClient {
 		private static final long ERROR_RESET_DELAY = 30000;
@@ -224,7 +236,7 @@ public class MainActivity extends AppCompatActivity implements View.OnSystemUiVi
 		@Override
 		public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
 			Log.d(TAG, "received error: "+error.getErrorCode()+" loading start page in "+ERROR_RESET_DELAY+" ms.");
-			_timerHandler.postDelayed(_errorResetRunnable, ERROR_RESET_DELAY);
+			_timerHandler.postDelayed(_errorResetRunnable, ERROR_RESET_DELAY); // reload page on error
 		}
 
 		@Override
@@ -233,8 +245,27 @@ public class MainActivity extends AppCompatActivity implements View.OnSystemUiVi
 		}
 
 		@Override
+		public void onPageFinished(WebView view, String url) {
+			Log.d(TAG, "Page finished.");
+			view.invalidate();
+			_webView.refreshDrawableState();	 // refresh visible UI after page load, just in case, might work without
+			super.onPageFinished(view, url);
+		}
+
+		@Override
 		public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
 			Log.d(TAG, "ssl error");
+			//super.onReceivedSslError(view, handler, error);
+			handler.proceed(); // skip SSL errors, should not do this, but helps when dealing with self-signed certificates
+		}
+
+		@Override
+		public void onPageStarted(WebView view, String url, Bitmap favicon) {
+			Log.d(TAG, "Injecting username "+USERNAME+" and password "+PASSWORD_HIDDEN);
+			/* inject data to HTML5 SessionStorage, in this case, username and password, we could also run other arbitrary JavaScript code after the page has finished loading if needed */
+			view.evaluateJavascript("sessionStorage.setItem(\'"+SESSIONSTORAGE_USERNAME+"\', \'"+USERNAME+"\')", null); // set credentials
+			view.evaluateJavascript("sessionStorage.setItem(\'"+SESSIONSTORAGE_PASSWORD+"\', \'"+PASSWORD+"\')", null); // set credentials
+			super.onPageStarted(view, url, favicon);
 		}
 	} // class WebClient
 }
